f2d5484b544b99e8e55545ca2a5a65513acf22f0
jerrydai90 <Jerrydai90@gmail.com>
2022-07-13 21:23:44 +0800

    rocketmq

---
+++ rocketmq/src/main/java/com/practice/rocketmq/SyncProducer2.java
@@ -0,0 +1,47 @@
+package com.practice.rocketmq;
+
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.common.message.MessageQueue;
+import org.apache.rocketmq.remoting.common.RemotingHelper;
+
+import java.util.List;
+
+public class SyncProducer2 {
+    public static void main(String[] args) throws Exception {
+        // 实例化消息生产者Producer
+        DefaultMQProducer producer = new DefaultMQProducer("test-d-p-group");
+        // 设置NameServer的地址
+        producer.setNamesrvAddr("localhost:9876");
+        // 启动Producer实例
+        producer.start();
+
+
+
+        for (int i = 0; i < 10000000; i++) {
+            // 创建消息，并指定Topic，Tag和消息体
+            Message msg = new Message("d-topic" /* Topic */,
+                    "d-tag" /* Tag */,
+                    ("Hello RocketMQ " + i).getBytes(RemotingHelper.DEFAULT_CHARSET) /* Message body */
+            );
+            // 发送消息到一个Broker
+            SendResult sendResult = producer.send(msg);
+
+
+            List<MessageQueue> messageQueues = producer.fetchPublishMessageQueues("d-topic");
+
+//            messageQueues.get(0).
+//
+//
+//
+//            producer.send(msg, )
+
+
+            // 通过sendResult返回消息是否成功送达
+//            System.out.printf("%s%n", sendResult);
+        }
+        // 如果不再发送消息，关闭Producer实例。
+        producer.shutdown();
+    }
+}
